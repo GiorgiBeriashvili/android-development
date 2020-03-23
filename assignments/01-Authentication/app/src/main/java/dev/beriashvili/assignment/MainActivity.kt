fbeec6649d93be12560d4da9e8093aa77d5c0ff0
Giorgi Beriashvili <giorgi.beriashvili@outlook.com>
2020-03-23 05:46:48 +0400

    Add the Authentication project
    This project serves as the first official completed assignment.

---
+++ assignments/01-Authentication/app/src/main/java/dev/beriashvili/assignment/MainActivity.kt
@@ -0,0 +1,27 @@
+package dev.beriashvili.assignment
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    private fun init() {
+        logInButton.setOnClickListener {
+            if (emailEditText.text.isNotBlank() && passwordEditText.text.isNotBlank()) {
+                Toast.makeText(this, "You have signed in successfully!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Please enter your account credentials!", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+}
